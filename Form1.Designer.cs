e83312af1f2124a08affd976bb8eb2f1638b17ea
MaikolCode <maikol320434@gmail.com>
2021-03-18 19:23:00 -0500

    Se agrega la primera versión

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace TallerWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTerceros = new System.Windows.Forms.RadioButton();
+            this.btnBasico = new System.Windows.Forms.RadioButton();
+            this.btnTotal = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Equipamiento = new System.Windows.Forms.GroupBox();
+            this.chkAudio = new System.Windows.Forms.CheckBox();
+            this.chkAire = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.btnCotizar = new System.Windows.Forms.Button();
+            this.txtCotizacion = new System.Windows.Forms.TextBox();
+            this.alert = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.Equipamiento.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(72, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(127, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seguro";
+            // 
+            // btnTerceros
+            // 
+            this.btnTerceros.AutoSize = true;
+            this.btnTerceros.Location = new System.Drawing.Point(78, 47);
+            this.btnTerceros.Name = "btnTerceros";
+            this.btnTerceros.Size = new System.Drawing.Size(77, 17);
+            this.btnTerceros.TabIndex = 3;
+            this.btnTerceros.Text = "A Terceros";
+            this.btnTerceros.UseVisualStyleBackColor = true;
+            // 
+            // btnBasico
+            // 
+            this.btnBasico.AutoSize = true;
+            this.btnBasico.Checked = true;
+            this.btnBasico.Location = new System.Drawing.Point(78, 24);
+            this.btnBasico.Name = "btnBasico";
+            this.btnBasico.Size = new System.Drawing.Size(57, 17);
+            this.btnBasico.TabIndex = 4;
+            this.btnBasico.TabStop = true;
+            this.btnBasico.Text = "Basico";
+            this.btnBasico.UseVisualStyleBackColor = true;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.AutoSize = true;
+            this.btnTotal.Location = new System.Drawing.Point(78, 71);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(49, 17);
+            this.btnTotal.TabIndex = 5;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTotal);
+            this.groupBox1.Controls.Add(this.btnBasico);
+            this.groupBox1.Controls.Add(this.btnTerceros);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(25, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 119);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seguro a seleccionar";
+            // 
+            // Equipamiento
+            // 
+            this.Equipamiento.Controls.Add(this.chkAudio);
+            this.Equipamiento.Controls.Add(this.chkAire);
+            this.Equipamiento.Controls.Add(this.label3);
+            this.Equipamiento.Location = new System.Drawing.Point(294, 80);
+            this.Equipamiento.Name = "Equipamiento";
+            this.Equipamiento.Size = new System.Drawing.Size(228, 119);
+            this.Equipamiento.TabIndex = 7;
+            this.Equipamiento.TabStop = false;
+            this.Equipamiento.Text = "Equipamiento";
+            // 
+            // chkAudio
+            // 
+            this.chkAudio.AutoSize = true;
+            this.chkAudio.Location = new System.Drawing.Point(89, 47);
+            this.chkAudio.Name = "chkAudio";
+            this.chkAudio.Size = new System.Drawing.Size(107, 17);
+            this.chkAudio.TabIndex = 4;
+            this.chkAudio.Text = "Sistema de audio";
+            this.chkAudio.UseVisualStyleBackColor = true;
+            // 
+            // chkAire
+            // 
+            this.chkAire.AutoSize = true;
+            this.chkAire.Location = new System.Drawing.Point(89, 21);
+            this.chkAire.Name = "chkAire";
+            this.chkAire.Size = new System.Drawing.Size(117, 17);
+            this.chkAire.TabIndex = 1;
+            this.chkAire.Text = "Aire acondicionado";
+            this.chkAire.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Equipo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Costo";
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(62, 230);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(118, 20);
+            this.txtCosto.TabIndex = 9;
+            this.txtCosto.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCotizar
+            // 
+            this.btnCotizar.Location = new System.Drawing.Point(294, 228);
+            this.btnCotizar.Name = "btnCotizar";
+            this.btnCotizar.Size = new System.Drawing.Size(75, 23);
+            this.btnCotizar.TabIndex = 10;
+            this.btnCotizar.Text = "Cotizar";
+            this.btnCotizar.UseVisualStyleBackColor = true;
+            this.btnCotizar.Click += new System.EventHandler(this.btnCotizar_Click);
+            // 
+            // txtCotizacion
+            // 
+            this.txtCotizacion.Location = new System.Drawing.Point(25, 276);
+            this.txtCotizacion.Multiline = true;
+            this.txtCotizacion.Name = "txtCotizacion";
+            this.txtCotizacion.Size = new System.Drawing.Size(524, 162);
+            this.txtCotizacion.TabIndex = 11;
+            // 
+            // alert
+            // 
+            this.alert.AutoSize = true;
+            this.alert.ForeColor = System.Drawing.Color.Red;
+            this.alert.Location = new System.Drawing.Point(256, 24);
+            this.alert.Name = "alert";
+            this.alert.Size = new System.Drawing.Size(0, 13);
+            this.alert.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::TallerWindows.Properties.Resources.engranaje;
+            this.pictureBox1.Location = new System.Drawing.Point(581, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 451);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.alert);
+            this.Controls.Add(this.txtCotizacion);
+            this.Controls.Add(this.btnCotizar);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Equipamiento);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Autos Corp.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Equipamiento.ResumeLayout(false);
+            this.Equipamiento.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton btnTerceros;
+        private System.Windows.Forms.RadioButton btnBasico;
+        private System.Windows.Forms.RadioButton btnTotal;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox Equipamiento;
+        private System.Windows.Forms.CheckBox chkAire;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkAudio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Button btnCotizar;
+        private System.Windows.Forms.TextBox txtCotizacion;
+        private System.Windows.Forms.Label alert;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TallerWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCotizar_Click(object sender, EventArgs e)
+        {
+            double costo = 0.0;
+            string cotizacion = "";
+
+            cotizacion = "Cotizacion de auto para " + txtName.Text + "\r\n";
+            cotizacion += "----------------------------------------------\r\n";
+
+            try
+            {
+                costo = Convert.ToDouble(txtCosto.Text);
+                if (btnBasico.Checked == true)
+                {
+                    costo += 500.0;
+                    cotizacion += "Lleva seguro basico de $500 \r\n";
+                }
+                if (btnTerceros.Checked == true)
+                {
+                    costo += 700.0;
+                    cotizacion += "Lleva seguro a Terceros de $700 \r\n";
+                }
+                if (btnTotal.Checked == true)
+                {
+                    costo += 1000.0;
+                    cotizacion += "Lleva seguro Total de $1000 \r\n";
+                }
+                if (chkAire.Checked == true)
+                {
+                    costo += 500.0;
+                    cotizacion += "Con Aire acondicionado de $500 \r\n";
+                }
+                if (chkAudio.Checked == true)
+                {
+                    costo += 700.0;
+                    cotizacion += "Con sistema de audio de $700 \r\n";
+                }
+                cotizacion += "\r\nEl total a pagar es de $" + costo.ToString();
+                cotizacion += "\r\n----------------------------------------------\r\n";
+                cotizacion += "Gracias por su cotización";
+                txtCotizacion.Text = cotizacion;
+            }
+            catch(Exception ex)
+            {
+                alert.Text = ($"Error: {ex}");
+            }
+        }
+    }
+}
